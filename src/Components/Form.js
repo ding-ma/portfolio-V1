21c5df11381191f433fcbc897195b2ea5ce9c44d
Ding Ma <ding.ma@mail.mcgill.ca>
2020-03-28 11:33:26 -0400

    key found

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -9,7 +9,7 @@ class Form extends Component {
                     id="form2"
                     name="form2"
                     className="wufoo  page"
-                    accept-charset="UTF-8"
+                    acceptCharset="UTF-8"
                     autoComplete="off"
                     encType="multipart/form-data"
                     method="post"
@@ -17,16 +17,13 @@ class Form extends Component {
                     action="https://dingma.wufoo.com/forms/ma5u4611pc2t7b/#public"
                 >
 
-                    <header id="header" className="info">
-                        <h2 className="">Contact Me</h2>
-                        <div className=""></div>
-                    </header>
+
                     <ul>
-                        <li id="foli4" data-wufoo-field data-field-type="text" className="notranslate      ">
+                        <li id="foli4" data-wufoo-field="" data-field-type="text" className="notranslate      ">
                             <label className="desc" id="title4" htmlFor="Field4">
                             </label>
                             <div>
-                                <input id="Field4" name="Field4" type="text" className="field text medium" value=""
+                                <input id="Field4" name="Field4" type="text" className="field text medium"
                                        maxLength="255" tabIndex="0" onKeyUp="" placeholder="Name"/>
                             </div>
                         </li>
@@ -35,7 +32,7 @@ class Form extends Component {
                             </label>
                             <div>
                                 <input id="Field2" name="Field2" type="email" spellCheck="false"
-                                       className="field text large" value="" maxLength="255" tabIndex="0"
+                                       className="field text large" maxLength="255" tabIndex="0"
                                        placeholder="Email"/>
                             </div>
                         </li>
@@ -43,7 +40,7 @@ class Form extends Component {
                             <label className="desc" id="title5" htmlFor="Field5">
                             </label>
                             <div>
-                                <input id="Field5" name="Field5" type="text" className="field text medium" value=""
+                                <input id="Field5" name="Field5" type="text" className="field text medium"
                                        maxLength="255" tabIndex="0" onKeyUp="" placeholder="Subject"/>
                             </div>
                         </li>
@@ -53,15 +50,15 @@ class Form extends Component {
                         </label>
 
                             <div>
-<textarea id="Field1"
-          name="Field1"
-          className="field textarea large"
-          spellCheck="true"
-          rows="10" cols="50"
-          tabIndex="0"
-          onKeyUp=""
-          placeholder="Message"
-></textarea>
+                                <textarea id="Field1"
+                                          name="Field1"
+                                          className="field textarea large"
+                                          spellCheck="true"
+                                          rows="10" cols="50"
+                                          tabIndex="0"
+                                          onKeyUp=""
+                                          placeholder="Message"
+                                />
 
                             </div>
                         </li>
@@ -76,8 +73,6 @@ class Form extends Component {
                         </li>
 
                         <li className="hide">
-                            <label htmlFor="comment">Do Not Fill This Out</label>
-                            <textarea name="comment" id="comment" rows="1" cols="1"></textarea>
                             <input type="hidden" id="idstamp" name="idstamp"
                                    value="mXve9YwHNaBnKLA9/3TzwVMOQYK+l03anPJsAL+lnUg="/>
                         </li>
